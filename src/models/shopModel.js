5c17d1063f91aabc2bc4a22726684be555ad33c1
JonCroatan <43881219+jonCroatanUto@users.noreply.github.com>
2021-12-15 13:27:24 +0100

    change the shop model , to include latitude and longitude with the same format of the api give us the data-->done; add requied to all the propierties of shop collection. If for some reason the api data and my databse data doesnt match , the requierd will show the reason of the error

--- src/models/shopModel.js
+++ src/models/shopModel.js
@@ -6,6 +6,10 @@ const ShopSchema = new Schema(
     name: {
       type: String,
       default: "",
+      require: [
+        true,
+        "We need the name for the correct functioning of the app",
+      ],
     },
     businessType: {
       type: String,
@@ -15,31 +19,59 @@ const ShopSchema = new Schema(
     vicinity: {
       type: String,
       default: "",
-      require: [true, "the shop adress is not setted"],
+      require: [
+        true,
+        "We need the adress for the correct functioning of the app",
+      ],
     },
     rating: {
       type: Number,
+      require: [
+        true,
+        "We need the rating for the correct functioning of the app",
+      ],
     },
     user_ratings_total: {
       type: Number,
+      require: [
+        true,
+        "We need the total user ratings for the correct functioning of the app",
+      ],
     },
-    latitude: {
-      type: Number,
-      default: "",
-      require: [true, "the latitude adress is not setted"],
+    geometry: {
+      location: {
+        lat: {
+          type: Number,
+          require: [
+            true,
+            "We need to save the latitude of the place, to the correct functioning of the app",
+          ],
+        },
+        lng: {
+          type: Number,
+          required: [
+            true,
+            "We need to save the longitude of the place, to the correct functioning of the app",
+          ],
+        },
+      },
     },
-    longitude: {
-      type: Number,
-      default: "",
-      require: [true, "the longitude adress is not setted"],
-    },
+
     icon: {
       type: String,
+      require: [
+        true,
+        "We need the icon for the correct functioning of the app",
+      ],
     },
     owner: [
       {
         type: Schema.Types.ObjectId,
         ref: "users",
+        require: [
+          true,
+          "We need to set a owner for the correct functioning of the app",
+        ],
       },
     ],
   },
